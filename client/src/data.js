6e61f831fc58d30587ad101cca12a7f67231d90d
nidhimantri28 <nidhimantri28@gmail.com>
2022-10-03 18:24:24 +0530

    change 2

--- client/src/data.js
+++ client/src/data.js
@@ -38,7 +38,7 @@ export const sliderItems = [
     {
       id: 1,
       img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "home st!",
+      title: "kartikey!",
       cat:"women"
     },
     {
